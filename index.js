f61a44d39d6e96e5573f92328d3cf33550d9d14c
Samuel Cornet <samuel@noukod.com>
2019-02-06 17:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,9 @@ BoardMember.prototype.releasePressStatement  = function() {
 };
 
 BoardMember.prototype.sayHi = function() {
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.';
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 };
+//   this.sayHi = function(){
+//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+//   }
+// }
